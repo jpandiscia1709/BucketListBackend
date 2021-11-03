e10e227ed88eb70016779285cc225ad25eb6d34c
jpandiscia1709 <jpangithub1709@gmail.com>
2021-11-03 17:21:47 -0400

    Created Bucketlist App + created Bucketlist model + registered Bucketlist model with admin site + migrated new bucketlist table

---
+++ drf_jwt_capstone_backend/bucketlist/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+
+# Create your models here.
+class Bucketlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    adventure = models.CharField(max_length=100)
